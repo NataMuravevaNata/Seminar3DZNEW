d8f9279b471febc921fddeaec47b3d38abcd34e2
Nata <muravevantsh@mail.ru>
2022-08-15 02:38:22 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,10 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+void FillArray (int[]array)
+{
+    int index=0;
+    while (index<array.Length)
+    {
+        array[index]=new Random().Next
+    }
+}
