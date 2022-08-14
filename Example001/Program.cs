14fa85bf6ad34c1baab177e05f6ddd6d80df4714
Nata <muravevantsh@mail.ru>
2022-08-14 12:50:08 +0300

    Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int []array = {1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1};
+int n = array.Length;
+int index = 0;
+while (index<n)
+{
+    Console.Write($"{array[index]}, ");
+    index++;
+}    
