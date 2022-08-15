fdfa9763a6fa249c58da3a29da79e5a8160815fb
Nata <muravevantsh@mail.ru>
2022-08-16 00:22:42 +0300

    Example006

--- Example006/Program.cs
+++ Example006/Program.cs
@@ -3,8 +3,39 @@
 void FillArray (int[]array)
 {
     int index=0;
-    while (index<array.Length)
+    while (index < array.Length)
     {
-        array[index]=new Random().Next
+        array[index]=new Random().Next(0,1000);
+        index++;
     }
 }
+
+void PrintArray (int[]array)
+{
+    int index=0;
+    while (index < array.Length)
+    {
+        Console.Write($"{array[index]}, ");
+        index++;
+    }
+}
+
+
+int[]array = new int [123];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
+
+int count=0;
+int i =1;
+    while (i<array.Length)
+    {
+        if (array [i] >=10 && array[i] <= 99)
+        count = count + i;
+        i++;
+    }
+    
+Console.WriteLine("Количество элементов из отрезка от 10 до 99:");    
+Console.WriteLine(count); 
+
+
