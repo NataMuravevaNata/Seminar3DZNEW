4616c80fe691e2b7cffc79e7d1d9f1d5b4011221
Nata <muravevantsh@mail.ru>
2022-08-16 00:21:44 +0300

    Example007

---
+++ Example007/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int [] array = {1, 5, 8, 7, 12, 24, 4, 7, 9, 1, 12};
+int n = array.Length;
+int counter = 1;
+int sum=0;
+Console.WriteLine("сумма чисел стоящих на нечетной позиции:"); 
+
+while (counter < n)
+{
+    sum = sum+array[counter];
+    counter=counter +2;
+}
+Console.WriteLine(sum);
